a854421fa94bf5c5f25ecc5ec91a6fe244bee487
Denis Shkirja <denis.shkirja@gmail.com>
2015-05-08 11:36:44 +0300

    change v,vt,vn parsing

--- obj_model.cpp
+++ obj_model.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
+#include <cctype>
 #include <regex>
 #include <stdexcept>
 #include <fstream>
 #include <string>
 #include "obj_model.h"
 
-static void concatinate_syserrmsg(std::string &errMsg)
+static
+std::string &rtrim(std::string &str)
+{
+    auto f = [](unsigned char const c) { return std::isgraph(c); };
+    str.erase(std::find_if(str.rbegin(), str.rend(), f).base(), str.end());
+    return str;
+}
+
+static
+std::string &ltrim(std::string &str)
+{
+    auto f = [](unsigned char const c) { return std::isgraph(c); };
+    str.erase(str.begin(), std::find_if(str.begin(), str.end(), f));
+    return str;
+}
+
+static
+std::string &trim(std::string &str)
+{
+    return rtrim(ltrim(str));
+}
+
+static
+void concatinate_syserrmsg(std::string &errMsg)
 {
     char sys_err_msg[256];
     if (strerror_r(errno, sys_err_msg, sizeof(sys_err_msg)))
@@ -29,116 +53,64 @@ ObjModel::ObjModel(const char *p_filePath)
         throw std::runtime_error(err_msg);
     }
 
-    std::string float_regex = R"(((?:\+|-)?[0-9]+(?:\.[0-9]+)?(?:(?:e|E)(?:\+|-)?[0-9]+)?))";
-    std::regex vertices_regex(R"(^(v(?:[tnp])?)[\s]+)" + float_regex +    // compulsory field
-                                R"((?:[\s]+)" + float_regex + R"()?)" +   // optional fields
-                                R"((?:[\s]+)" + float_regex + R"()?)" +
-                                R"((?:[\s]+)" + float_regex + R"()?)" +
-                              R"([\s]*$)");
-    std::string geom_tex_norm_v = R"(([0-9]+)(?:(?:\/\/([0-9]+))|(?:\/([0-9]+)(?:\/([0-9]+))?))?)";
-    std::regex faces_regex(R"(^f(([\s]+)" + geom_tex_norm_v + R"(){3,})[\s]*$))");
-    std::regex comment_regex(R"(^[\s]*#.*$)");
-
-    std::string in_str;
-    while (std::getline(in_file, in_str))
+    std::string line;
+    unsigned long line_number = 0;
+    while (std::getline(in_file, line))
     {
-        std::smatch match;
+        line_number++;
+        trim(line);
 
-        if (in_str.empty() || std::regex_match(in_str, match, comment_regex))
+        if (line.empty() || line[0] == '#')
         {
             continue;
         }
-        else if (std::regex_match(in_str, match, vertices_regex))
-        {
-            if (match[1] == "v")
-            {
-                float x = std::stof(match[2]);
-                float y = std::stof(match[3]);
-                float z = std::stof(match[4]);
 
-                Vector3f vect(x, y, z);
-                _VerticesGeometric.push_back(vect);
-            }
-            else if (match[1] == "vt")
+        std::istringstream stream(line);
+
+        if (line[0] == 'v' && std::isspace(line[1]))
+        {
+            stream.ignore(2);
+            std::vector<float> values(std::istream_iterator<float>{stream}, std::istream_iterator<float>{});
+
+            if (values.size() != 3 || !stream.eof())
             {
+                std::cout << "Error: worng input on line " << line_number << std::endl;
             }
-            else if (match[1] == "vn")
+            else
             {
+                Vector3f v(values[0], values[1], values[2]);
+                _VerticesGeometric.push_back(v);
             }
-            else if (match[1] == "vp")
+        }
+        else if (line[0] == 'v' && line[1] == 't' && std::isspace(line[2]))
+        {
+            stream.ignore(3);
+            std::vector<float> values(std::istream_iterator<float>{stream}, std::istream_iterator<float>{});
+
+            if (values.size() != 3 || !stream.eof())
+            {
+                std::cout << "Error: worng input on line " << line_number << std::endl;
+            }
+            else
             {
             }
         }
-        else if(std::regex_match(in_str, match, faces_regex))
+        else if (line[0] == 'v' && line[1] == 'n' && std::isspace(line[2]))
         {
-            std::string face = match[1];
+            stream.ignore(3);
+            std::vector<float> values(std::istream_iterator<float>{stream}, std::istream_iterator<float>{});
 
-            std::vector<unsigned long> face_v_indices;
-            std::vector<unsigned long> face_vt_indices;
-            std::vector<unsigned long> face_vn_indices;
-
-            bool error = false;
-            std::regex face_elements(geom_tex_norm_v);
-            while (std::regex_search(face, match, face_elements))
+            if (values.size() != 3 || !stream.eof())
             {
-                unsigned long v = 0;
-                unsigned long vt = 0;
-                unsigned long vn = 0;
-
-                if (match[6].str().empty() == false)
-                {
-                    v = std::stoul(match[6]) - 1;
-                    vt = std::stoul(match[7]) - 1;
-                    vn = std::stoul(match[8]) - 1;
-                    face_v_indices.push_back(v);
-                    face_vt_indices.push_back(vt);
-                    face_vn_indices.push_back(vn);
-                }
-                else if (match[4].str().empty() == false)
-                {
-                    v = std::stoul(match[4]) - 1;
-                    vn = std::stoul(match[5]) - 1;
-                    face_v_indices.push_back(v);
-                    face_vn_indices.push_back(vn);
-                }
-                else if (match[2].str().empty() == false)
-                {
-                    v = std::stoul(match[2]) - 1;
-                    vt = std::stoul(match[3]) - 1;
-                    face_v_indices.push_back(v);
-                    face_vt_indices.push_back(vt);
-                }
-                else if (match[1].str().empty() == false)
-                {
-                    v = std::stoul(match[1]) - 1;
-                    face_v_indices.push_back(v);
-                }
-                else
-                {
-                    std::cerr << "\nCan not find indices in face match '" << face << "'.";
-                    error = true;
-                    break;
-                }
-
-                face = match.suffix();
+                std::cout << "Error: worng input on line " << line_number << std::endl;
             }
-
-            if (error == false)
+            else
             {
-                _FacesVertex.push_back(face_v_indices);
-                if (face_vt_indices.empty() == false)
-                {
-                    _FacesTexture.push_back(face_vt_indices);
-                }
-                if (face_vn_indices.empty() == false)
-                {
-                    _FacesNormal.push_back(face_vn_indices);
-                }
             }
         }
-        else
+        else if (line[0] == 'f' && std::isspace(line[1]))
         {
-            std::cerr << "Can not parse line '" << in_str << "' in file '" << p_filePath << "'.\n";
+            ;
         }
     }
     if (!in_file.eof())
